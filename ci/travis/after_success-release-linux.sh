0cd8ae53bbba635399b118a1a9e1b1a761387e2a
Matthew Taylor <matt@numenta.org>
2015-01-06 12:47:01 -0800

    Testing wheel platforms

---
+++ ci/travis/after_success-release-linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+echo
+echo "Running after_success-release.sh..."
+echo
+
+pip --version
+
+echo "Installing wheel..."
+pip install wheel || exit
+echo "Installing twine..."
+pip install twine || exit
+
+python -c "import wheel; from wheel.pep425tags import get_supported; print ['-'.join(t) for t in get_supported()]"
